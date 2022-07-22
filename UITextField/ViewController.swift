a52f220b480a215204dba3709923727af3e19320
damiru2 <chalkarov11@mail.ru>
2022-07-22 21:24:33 +0500

    Initial Commit

---
+++ UITextField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextField
+//
+//  Created by Damir Chalkarov on 22.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
